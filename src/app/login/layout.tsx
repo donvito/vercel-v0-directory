e019b2c902203a5149a2845f28082bafbd8bfeb9
Melvin Vivas <melvindave@gmail.com>
2024-07-03 16:25:29 +0800

    added initial components

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { HeaderNavigation } from "@/components/header-navigation";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,11 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <HeaderNavigation />
+    <div>
         {children}
-      </body>
-    </html>
+    </div>
   );
 }
