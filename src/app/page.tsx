12ed097bfd452779b43e974b8d9228aebed3f98a
Melvin Vivas <melvindave@gmail.com>
2024-07-16 12:11:57 +0800

    added initial impl of nextauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,37 @@
+// mark as client component
+"use client";
+
+// importing necessary functions
+import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image";
+
 import HeroMain from "@/components/HeroMain";
 import InstructionsSectionCode from "@/components/InstructionsSectionCode";
 import PricingSection from "@/components/PricingSection";
 import TestimonialSection from "@/components/TestimonialsSection";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col justify-between p-8">        
-      <HeroMain />
-      <InstructionsSectionCode />
-      <TestimonialSection />
-      <PricingSection />
-    </main>
-  );
+    // extracting data from usesession as session
+    const { data: session } = useSession()
+
+    // checking if sessions exists
+    if (session) {
+        // rendering components for logged in users
+        return (
+        <>
+        <p>Welcome {session.user?.name}. Signed In As</p>
+        <p>{session.user?.email}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+        </>
+        )
+    }
+
+    return (
+        <>
+        //<main className="flex min-h-screen flex-col justify-between p-8">
+            <p>Not Signed In</p>
+            <button onClick={() => signIn('github')}>Sign in with github</button>
+        //</main>
+        </>
+    );
 }
