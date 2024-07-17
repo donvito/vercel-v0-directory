dc0ae2bb5a014129c673ec62b093c307f82d1e9b
Melvin Vivas <melvindave@gmail.com>
2024-07-17 14:10:21 +0800

    fix login height

--- src/components/LoginSocial.tsx
+++ src/components/LoginSocial.tsx
@@ -14,7 +14,7 @@ export default async function LoginSocial() {
     }
 
     return (
-        <div className="min-h-screen bg-base-200 flex items-center">
+        <div className="min-h-[70vh] bg-base-200 flex items-center">
             <div className="card mx-auto w-full max-w-xs shadow-2xl bg-base-100">
                 <div className="card-body">
                     <h2 className="card-title text-2xl font-bold mb-2">You need to sign in</h2>
